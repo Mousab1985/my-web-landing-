5cca08861937975c6e5a3630e1a212052f796eb7
Mousab1985 <144440430+Mousab1985@users.noreply.github.com>
2024-08-29 16:14:29 +0200

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Button from "./Button";
+import Nav from "./Nav";
+import ShopCard from "./ShopCard";
+import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
+
+export { Button, Nav, ShopCard, PopularProductCard, ServiceCard, ReviewCard };
